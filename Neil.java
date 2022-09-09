6883b524c27c1cc0ba9c74e37e431b2b13f8ed75
Neil-Francis-Navarro <neilfrancis.navarro@students.isatu.edu.ph>
2022-09-09 17:40:23 +0800

    first commit

---
+++ Neil.java
@@ -0,0 +1,14 @@
+public class Neil
+{
+		public static void main(String[] args)
+		{
+			System.out.println("\n\t\t\t\t\"Personal Profile\"\n\n\n");
+
+			System.out.println("\t\tName: Neil Francis N. Navarro \t\tBirthday: April 20,2001\n\n");
+	
+			System.out.println("\t\tAge: 21 years old \t\t\tEmail: neilfrancis.navarro@students.isatu.edu.ph\n\n");
+			
+			System.out.println("\t\tSex: Male \t\t\t\tContact#: 09083851709");
+			
+		}
+}
